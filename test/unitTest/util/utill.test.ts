723c0ff74ec957f675592aaa1449dbf3fb4114d3
narongsak keawmanee <klogic@hotmail.co.th>
2020-06-14 13:29:10 +0700

    feat: 🎸 add sinon stub

--- test/unitTest/util/utill.test.ts
+++ test/unitTest/util/utill.test.ts
@@ -1,9 +1,6 @@
 import sinon from "sinon";
 import * as util from "../../../src/util/util";
 const fs = require("fs");
-const fakeReturn = sinon.fake.returns("hi world");
-const fakeThrow = sinon.fake.throws(new Error("error here"));
-const fakeYield = sinon.fake.yields(null, "fakeYield World");
 
 afterEach(() => {
   sinon.restore();
@@ -11,22 +8,32 @@ afterEach(() => {
 
 describe("testFunction function", () => {
   test("testFunction should return hello world", () => {
+    const fakeReturn = sinon.fake.returns("hi world");
     sinon.replace(util, "testFunction", fakeReturn);
     expect(util.testFunction()).toEqual("hi world");
     expect(fakeReturn.callCount).toEqual(1);
   });
 
   test("testFunction should throw error here", () => {
+    const fakeThrow = sinon.fake.throws(new Error("error here"));
     sinon.replace(util, "testFunction", fakeThrow);
     expect(util.testFunction).toThrowError(new Error("error here"));
     expect(fakeThrow.callCount).toEqual(1);
   });
 
   test("testFunctionFunction should return fakeYield World", () => {
+    const fakeYield = sinon.fake.yields(null, "fakeYield World");
     sinon.replace(fs, "readFile", fakeYield);
     fs.readFile("somefile", (err: string, data: string) => {
       expect(data).toEqual("fakeYield World");
     });
     expect(fakeYield.callCount).toEqual(1);
+  });
+});
+
+describe("testFunction function Stub", () => {
+  test("testFunction should return hello world", () => {
+    sinon.stub(util, "testFunction").returns("hi world");
+    expect(util.testFunction()).toEqual("hi world");
   });
 });
